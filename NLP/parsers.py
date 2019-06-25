333035b3bb8d061a3e4dcc86954b07bc91381584
Cherkasov Egor <cherkasovoff@mail.ru>
2019-06-25 08:44:49 +0500

    nlp upd

--- NLP/parsers.py
+++ NLP/parsers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,17 +16,13 @@ class MeduzaParser(BaseParser):
 
     url = 'https://meduza.io/api/v3/search?chrono=news&locale=ru&page={page}&per_page=24'
 
-    
-
     def parse_news(self, count=5000):
         news = []
         cur_page = 1
 
         while count > 0:
             try:
-                page_news = self.get_page_data(page=cur_page,
-                                               url=self.url,
-                                               user_agent=self.USER_AGENT)
+                page_news = self.get_page_data(page=cur_page)
                 news.extend(page_news)
                 count -= len(page_news)
                 cur_page += 1
@@ -35,9 +31,9 @@ class MeduzaParser(BaseParser):
 
         return news
 
-    def get_page_data(self, page, url, user_agent, site_url="https://meduza.io/{0}"):
-        headers = {'User-agent' : user_agent}
-        response = requests.get(url.format(page=page), headers=headers).json()
+    def get_page_data(self, page, site_url="https://meduza.io/{0}"):
+        headers = {'User-agent' : self.USER_AGENT}
+        response = requests.get(self.url.format(page=page), headers=headers).json()
         
         result = []
         urls = [site_url.format(key) for key in response['documents'].keys()]
@@ -99,6 +95,62 @@ class MeduzaParser(BaseParser):
         return date
 
 
+class LentaParser(BaseParser):
+
+    base_url = 'https://lenta.ru/'
+
+    def parse_news(self, count=5000):
+        news = []
+        date_delta = 0
+
+        while count > 0:
+            cur_date = str(date.today() - timedelta(days=date_delta)).replace('-','/')
+            try:
+                page_news = self.get_page_data(date=cur_date)
+                news.extend(page_news)
+                count -= len(page_news)
+                date_delta += 1
+            except:
+                traceback.print_exc()
+
+        return news
+
+    def get_page_data(self, date):
+        headers = {'User-agent' : self.USER_AGENT}
+        page_responce = requests.get(self.base_url + date)
+
+        page_content = BeautifulSoup(page_responce.content, "html.parser")
+        urls = ['https://lenta.ru' + i.a['href'] for i in page_content.find_all('div', {"class":"titles"})]
+
+        result = []
+        for url in urls:
+
+            page_responce = requests.get(url, headers=headers)
+            if page_responce.status_code == 200:
+                page_content = BeautifulSoup(page_responce.content, "html.parser")
+
+                try:
+                    title = page_content.find_all("h1")[0].text
+                except:
+                    continue
+
+                body = ' '.join([p.text for p in page_content.find_all('p')])
+
+                try:
+                    date = page_content.find("time", {"class": "g-date"})['datetime']
+                    date = datetime.strptime(date[:-6], '%Y-%m-%dT%H:%M:%S')
+                except:
+                    date = None
+
+                entry = {"author": None,
+                         "title" : title,
+                         "body" : body,
+                         "date": date}
+                result.append(entry)
+                
+        return result
+
+
 class AifParser(BaseParser):
 
     base_url = "http://www.aif.ru/{rubric}"
@@ -117,8 +169,7 @@ class AifParser(BaseParser):
             for rubric in self.rubrics:
                 try:
                     page_news = self.get_page_data(rubric=rubric,
-                                                   page=cur_page,
-                                                   user_agent=self.USER_AGENT)
+                                                   page=cur_page)
                     news.extend(page_news)
                     count -= len(page_news)
                 except:
@@ -128,8 +179,8 @@ class AifParser(BaseParser):
 
         return news
 
-    def get_page_data(self, rubric, page, user_agent):
-        headers = {'User-agent' : user_agent}
+    def get_page_data(self, rubric, page):
+        headers = {'User-agent' : self.USER_AGENT}
         page_responce = requests.post(self.base_url.format(rubric=rubric),
                                       headers=headers,
                                       data={"page": page})
